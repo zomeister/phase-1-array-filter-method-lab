8efb7f3b17b58200990dedb7ffa62f0f39e4fc28
Zoe Guidry <oyezoey@gmail.com>
2023-03-29 09:24:36 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, str) {
+  return arr.filter(element => element.toUpperCase() === str.toUpperCase())
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter(element => element.slice(0, str.length ) === str)
+}
+
+function matchName(arr, str) {
+  return arr.filter(element => element.name === str)
+}
+
